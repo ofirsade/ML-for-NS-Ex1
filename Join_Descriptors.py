6b2713ca9161abadc268b91bf305b10b8bfa0890
ofirsade <65113000+ofirsade@users.noreply.github.com>
2022-03-05 10:54:34 +0200

    Update Join_Descriptors.py

--- Join_Descriptors.py
+++ Join_Descriptors.py
@@ -9,24 +9,17 @@ pd.set_option('display.max_columns', None)
 
 input_path = '/Users/ofirsade/Desktop/UNI/Masters/Courses/SEMESTER I/למידה חישובית למדעי המוח/HW/Final Project/12868_2016_287_MOESM1_ESM.xlsx'
 
-input_path2 = '/Users/ofirsade/Desktop/UNI/Masters/Courses/SEMESTER I/למידה חישובית למדעי המוח/HW/Final Project/Molecule_Descriptors.csv'
+input_path2 = '/Users/ofirsade/Desktop/UNI/Masters/Courses/SEMESTER I/למידה חישובית למדעי המוח/HW/Final Project/Molecule_Descriptors.xlsx'
 
 
 df = pd.read_excel(input_path, header = 2)
 df["CAN OR CAN'T SMELL"] = (df["CAN OR CAN'T SMELL"] == "I smell something").astype(int)
 df["KNOW OR DON'T KNOW THE SMELL"] = (df["KNOW OR DON'T KNOW THE SMELL"] == 'I know what the odor is').astype(int)
 df['Gender'] = (df['Gender'] == 'M').astype(int)
-df1 = df[['SMILES', 'Odor', 'Odor dilution', 'Subject # (DREAM challenge) ', 'Gender', 'Age', "CAN OR CAN'T SMELL", "KNOW OR DON'T KNOW THE SMELL",
-          'HOW STRONG IS THE SMELL?', 'HOW PLEASANT IS THE SMELL?', 'HOW FAMILIAR IS THE SMELL?']]
-#print(df)
 
-df2 = pd.read_csv(input_path2)
+df2 = pd.read_excel(input_path2)
 
-#print("DF1 Columns: ", list(df.columns),
-#      "\nDF2 Columns: ", list(df2.columns))
-
-
-result_df = pd.merge(df1, df2, how = "left", on = "SMILES")
+result_df = pd.merge(df, df2, how = "left", on = "SMILES")
 
 file = open('/Users/ofirsade/Desktop/UNI/Masters/Courses/SEMESTER I/למידה חישובית למדעי המוח/HW/Final Project/All_Descriptors.xlsx',
             'w+', newline='')
@@ -34,31 +27,4 @@ output_path = '/Users/ofirsade/Desktop/UNI/Masters/Courses/SEMESTER I/למידה
 
 result_df.to_excel(output_path)
 
-#plt.figure(figsize=(12,10))
-
-#corr = result_df.corr(method = 'pearson')
-
-#sns.heatmap(corr, annot = True)
-
-#plt.show()
-
-
-
-
-"""
-col_names = ['Odor', 'Odor dilution', 'Gender',
-             'Race ("unknown" indicates that the subject did not wish to specify)', 'Ethnicity',
-             'Age', 'Detection (yes/no)', 'Familiarity (yes/no)', 'Intensity (0-100)',
-             'Pleasantness (0-100)', 'Familiarity (1-100)']
-
-df = pd.read_csv(input_path, usecols = col_names)
-
-col_names2 = ['MaxEStateIndex', 'MinEStateIndex', 'MaxAbsEStateIndex',
-              'MinAbsEStateIndex', 'qed', 'MolWt', 'HeavyAtomMolWt',
-              'ExactMolWt', 'NumValenceElectrons', 'NumRadicalElectrons',
-              'MaxPartialCharge', 'MinPartialCharge', 'MaxAbsPartialCharge',
-              'MinAbsPartialCharge', 'FpDensityMorgan1', 'FpDensityMorgan2',
-              'FpDensityMorgan3', 'BCUT2D_MWHI', 'BCUT2D_MWLOW',
-              'BCUT2D_CHGHI', 'BCUT2D_CHGLO']
-"""
 
