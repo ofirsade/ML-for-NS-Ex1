92cdfa3aedb0bfed18f7ef0d97016907e77df9b8
yonatanGoldstein <yonatang2@mail.tau.ac.il>
2022-03-12 12:07:34 +0200

    Added remarks

--- Data_preprocessing.py
+++ Data_preprocessing.py
@@ -7,14 +7,25 @@ from typing import List
 import pyrfume
 import pubchempy as pcp
 
-
+# specify the input path for the dataset
 input_path_data = r"C:\Users\dell\Documents\ML final project\ML-for-NS-Ex1\12868_2016_287_MOESM1_ESM.xlsx"
 
+# loading the dataset
 df2 = pd.read_excel(input_path_data, header=2)
+
+# keeping only index information and the 20 odor characters
 df2 = df2[["CID", "Odor","Odor dilution","EDIBLE ","BAKERY ","SWEET ","FRUIT ", "FISH", "GARLIC ","SPICES ", "COLD", "SOUR","BURNT ", "ACID ", "WARM ", "MUSKY ", "SWEATY ","AMMONIA/URINOUS","DECAYED","WOOD ","GRASS ","FLOWER ", "CHEMICAL "]]
+
+# removing spaces from columns names
 df2.columns = df2.columns.str.replace(' ', '')
+
+# changing 'Odordilution' to 'Odor_dilution
 df2.rename(columns={'Odordilution': 'Odor_dilution'}, inplace=True)
+
+# replacing na with 0 for future mean calculation
 df2.fillna(0,inplace=True)
+
+# replacing wrong CID's that contain "-"
 cid = df2['CID']
 odor = df2['Odor']
 for i in range(len(cid)):
@@ -22,10 +33,15 @@ for i in range(len(cid)):
         s = pcp.get_compounds(odor[i], 'name')
         cid[i] = s[0].cid
 df2["CID"] = cid
+
+# grouping the dataset by odor and dilution. each row will contain the mean of every odor charcter
 df2 = df2.groupby(['CID', 'Odor_dilution'], sort=False).mean()
+
+# finding the 3 max means for every combination of odor and dilution
 c = ['1st Max','2nd Max','3rd Max']
 df2 = df2.apply(lambda x: pd.Series(x.nlargest(3).index, index=c), axis=1).reset_index()
 
+# 1) adding "SMILES" to the df using, "CID". 2) for each row, checks if "SOUR" is in max 3 means. 
 y = []
 smiles=[]
 cid = df2['CID']
@@ -36,11 +52,14 @@ for i in range(len(df2)):
     smile = s.isomeric_smiles
     smiles.append(smile)
 
+# adding "SMILES" to the df
 df2["SMILES"] = smiles
 df2 = df2[["CID", "Odor_dilution", "SMILES"]]
+
+# dummy encoding odor dilution
 df2 = pd.get_dummies(df2, columns=['Odor_dilution'])
 
-
+# extract molecular descriptors
 class RDKit_2D:
     def __init__(self, smiles: pd.Series):
         self.smiles: pd.Series = smiles
@@ -63,14 +82,17 @@ smiles = data.IsomericSMILES
 kit = RDKit_2D(smiles)
 df = kit.compute_descriptors()
 
+# merging the df's using "SMILES"
 X = pd.merge(df2, df, how = "left", on = "SMILES")
 
+# saving y as pd.Series
 file_y = open(r'C:\Users\dell\Documents\ML final project\ML-for-NS-Ex1\y.xlsx',
             'w+', newline='')
 output_path_y = r'C:\Users\dell\Documents\ML final project\ML-for-NS-Ex1\y.xlsx'
 
 pd.Series(y).to_excel(output_path_y)
 
+# saving X as df
 file_X = open(r'C:\Users\dell\Documents\ML final project\ML-for-NS-Ex1\X.xlsx',
             'w+', newline='')
 output_path_X = r'C:\Users\dell\Documents\ML final project\ML-for-NS-Ex1\X.xlsx'
