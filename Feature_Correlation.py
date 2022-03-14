ca0e78398199c9fbe94344aa7411ee9a58cbc596
ofirsade <65113000+ofirsade@users.noreply.github.com>
2022-03-14 08:23:51 +0200

    Add files via upload

--- Feature_Correlation.py
+++ Feature_Correlation.py
@@ -1,41 +1,31 @@
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.preprocessing import StandardScaler
 
-#pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', None)
-#pd.set_option('display.width', None)
-#pd.set_option('display.max_colwidth', None)
 
-input_path = '/Users/ofirsade/Desktop/UNI/Masters/Courses/SEMESTER I/למידה חישובית למדעי המוח/HW/Final Project/All_Descriptors.csv'
 
-df = pd.read_csv(input_path)
+### Load the dataset
+input_path = input("Input path to file including file name: ")
+df = pd.read_excel(input_path)
+df = df[['MinEStateIndex', 'BCUT2D_MRLOW', 'BalabanJ', 'Chi4n', 'MolMR']]
+df = df.loc[:, df.any()]
+numeric_features = df.select_dtypes(include=np.number)
+df.loc[:, numeric_features.columns] = df.loc[:, numeric_features.columns].apply(lambda l: l.fillna(l.mean()),axis=0)
+scaler = StandardScaler()
 
+# Get scaling parameters with the train sample exclusively, using the Scaler.fit() function
+scaler.fit(df.loc[:, numeric_features.columns])
+
+
+# Scale data using Scaler.transform()
+df.loc[:, numeric_features.columns] = scaler.transform(df.loc[:, numeric_features.columns])
+
+
+### Create the correlation matrix
 plt.figure(figsize=(12,10))
-
 corr = df.corr(method = 'pearson')
-
-sns.heatmap(corr)#, annot = True)
-
+sns.heatmap(corr, annot = True)
 plt.show()
-
-
-
-
-"""
-col_names = ['Odor', 'Odor dilution', 'Gender',
-             'Race ("unknown" indicates that the subject did not wish to specify)', 'Ethnicity',
-             'Age', 'Detection (yes/no)', 'Familiarity (yes/no)', 'Intensity (0-100)',
-             'Pleasantness (0-100)', 'Familiarity (1-100)']
-
-df = pd.read_csv(input_path, usecols = col_names)
-
-col_names2 = ['MaxEStateIndex', 'MinEStateIndex', 'MaxAbsEStateIndex',
-              'MinAbsEStateIndex', 'qed', 'MolWt', 'HeavyAtomMolWt',
-              'ExactMolWt', 'NumValenceElectrons', 'NumRadicalElectrons',
-              'MaxPartialCharge', 'MinPartialCharge', 'MaxAbsPartialCharge',
-              'MinAbsPartialCharge', 'FpDensityMorgan1', 'FpDensityMorgan2',
-              'FpDensityMorgan3', 'BCUT2D_MWHI', 'BCUT2D_MWLOW',
-              'BCUT2D_CHGHI', 'BCUT2D_CHGLO']
-"""
-
